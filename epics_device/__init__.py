c78e4b4236b09bb82794b977e77cda708639c371
Michael Abbott <michael.abbott@diamond.ac.uk>
2021-09-13 09:03:29 +0100

    Fix compatibility with epicsdbbuilder from version 1.4
    Remove dependency on the ExportModules method now deleted from
    epicsdbbuilder

--- epics_device/__init__.py
+++ epics_device/__init__.py
@@ -1,8 +1,6 @@
+# Do everything required to load the epicsdbbuilder prerequisites
 from . import builder
 
-import epicsdbbuilder
-__all__ = epicsdbbuilder.ExportModules(globals(),
-    'epics_device', ['device', 'common'])
-
 from epicsdbbuilder import *
-__all__.extend(epicsdbbuilder.__all__)
+from .device import *
+from .common import *
